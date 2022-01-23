fafa6450607359b3c8e8fe2f3af678f35fc55690
NGENZI Herve <48619051+muhenge@users.noreply.github.com>
2022-01-23 17:14:47 +0200

    Add skill to edit

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -54,6 +54,6 @@ class SkillsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def skill_params
-      params.require(:skill).permit(:name, :level, :user_id)
+      params.permit(:name, :level, :user_id, :user_slug)
     end
 end
