dd74327d081511a891856de31fc3d4dd9c915e66
muhenge <mugunga5@gmail.com>
2022-02-03 17:07:22 +0200

    Add users cards

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show user_connections] 
+  before_action :set_user, only: %i[index show user_connections] 
   before_action :authenticate_user!
 
   def index
     @users = User.all
+    
   end
 
   def user_connections
